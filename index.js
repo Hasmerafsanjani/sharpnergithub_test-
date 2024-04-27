21b17909f39303d8686b0aeedf9351b97903cecb
Hasme Rafsanjani <hasmerafsanjani@gmail.com>
2024-04-27 20:20:16 +0530

    final

---
+++ index.js
@@ -0,0 +1,23 @@
+// function handleFormSubmit(event){
+//     event.preventDefault()
+//     console.log(event.target.name.value)
+//     console.log(event.target.email.value)
+//     console.log(event.target.telephone.value)
+//     console.log(event.target.date.value)
+//     console.log(event.target.time.value)
+//   }
+ var name = "hasme" 
+ var  age=29
+ var address= "cooch"
+
+
+//   console.log("personal details = My name is ", name , "age is", age," address is", address)
+
+
+// console.log("personal details = My name is " + name + "age is" + age + " address is" + address)
+
+
+// console.log(`${personal details = My name is ${name} age is ${age} address is ${address}`)
+
+
+console.log(`personal details = My name is ${name} age is ${age} address is ${address}`)
