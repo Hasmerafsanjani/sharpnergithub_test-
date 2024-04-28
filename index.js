179a41ad353fc4830daf5cdd3ce3824c5b361225
Hasme Rafsanjani <hasmerafsanjani@gmail.com>
2024-04-28 20:09:45 +0530

    made some changes

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-// function handleFormSubmit(event){
-//     event.preventDefault()
-//     console.log(event.target.name.value)
-//     console.log(event.target.email.value)
-//     console.log(event.target.telephone.value)
-//     console.log(event.target.date.value)
-//     console.log(event.target.time.value)
-//   }
- var name = "hasme" 
- var  age=29
- var address= "cooch"
-
-
-//   console.log("personal details = My name is ", name , "age is", age," address is", address)
-
-
-// console.log("personal details = My name is " + name + "age is" + age + " address is" + address)
-
-
-// console.log(`${personal details = My name is ${name} age is ${age} address is ${address}`)
-
-
-console.log(`personal details = My name is ${name} age is ${age} address is ${address}`)
+function handleFormSubmit(event){
+    event.preventDefault()
+    console.log(event.target.name.value)
+    console.log(event.target.email.value)
+    console.log(event.target.telephone.value)
+    console.log(event.target.date.value)
+    console.log(event.target.time.value)
+  }
+ 
